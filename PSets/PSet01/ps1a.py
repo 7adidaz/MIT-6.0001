553d4148165a847c44271a7949d3a5134dec37a9
Abdallah Elhadad <86894852+7adidaz@users.noreply.github.com>
2021-08-16 20:03:57 +0200

    Add files via upload

---
+++ PSets/PSet01/ps1a.py
@@ -0,0 +1,18 @@
+annual_salary = int(input("Enter your annual salary:"))
+# portion_saved is the amount saved every month for the house
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal:"))
+total_cost = int(input("Enter the cost of your dream home:"))
+current_saving = 0
+#month counter!
+counter=0
+portion_down_payment = .25*total_cost #saving for down payment
+month_salary = annual_salary/12
+
+while current_saving<portion_down_payment:
+    #the received funds to put into the saving
+    current_saving+=(current_saving*0.04)/12 
+    #salary saving each month for the down payment
+    current_saving+=portion_saved*month_salary
+    counter+=1  
+    
+print("Number of months:", counter)
---
+++ PSets/PSet01/ps1b.py
@@ -0,0 +1,22 @@
+annual_salary = int(input("Enter your annual salary:"))
+# portion_saved is the amount saved every month for the house
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal:"))
+total_cost = int(input("Enter the cost of your dream home:"))
+semi_raise = float(input("Enter the semi annual raise, as a decimal:"))
+
+current_saving = 0
+#month counter!
+counter=0
+portion_down_payment = .25*total_cost #saving for down payment
+month_salary = annual_salary/12
+
+while current_saving<portion_down_payment:
+    if counter%6==0 and counter !=0:
+        month_salary+=month_salary*semi_raise
+    #the received funds to put into the saving
+    current_saving+=(current_saving*0.04)/12 
+    #salary saving each month for the down payment
+    current_saving+=portion_saved*month_salary
+    counter+=1  
+    
+print("Number of months:", counter)
