d7626a4d3bf80e3c9cb801a9d5143ddf2f163ded
Abdallah Elhadad <86894852+7adidaz@users.noreply.github.com>
2021-08-16 21:23:36 +0200

    Update ps1b.py

--- PSets/PSet01/ps1b.py
+++ PSets/PSet01/ps1b.py
@@ -7,7 +7,7 @@ semi_raise = float(input("Enter the semi annual raise, as a decimal:"))
 current_saving = 0
 #month counter!
 counter=0
-portion_down_payment = .25*total_cost #saving for down payment
+portion_down_payment = .25*total_cost #down payment
 month_salary = annual_salary/12
 
 while current_saving<portion_down_payment:
